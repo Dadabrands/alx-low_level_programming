34affa10e0980e70cbc56c39eac46955e374b918
Dada Joshua <dadajoshua055@gmail.com>
2023-07-13 07:54:05 -0700

    third commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programmingis like building a multilingual purple");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Description: A program that print a line using use printf function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("With proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
