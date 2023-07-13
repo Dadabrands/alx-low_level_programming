c613e33598310cac390f44af4e9a5f0b83fbea46
Dada Joshua <dadajoshua055@gmail.com>
2023-07-13 08:32:26 -0700

    My 200 percent

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-/*
- * main - Entry point
- * Description: A program that print a line using use printf function
- * Return: Always 0 (Success)
+/**
+ * main - Entry of the program
+ * Return: Always returns (0)
  */
 
 int main(void)
 {
-printf("With proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
